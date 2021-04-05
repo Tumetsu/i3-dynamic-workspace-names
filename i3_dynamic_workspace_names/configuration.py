a1ddd8cf1e4b43edac02470615de6d9d0f99a8ce
Tuomas Salmi <salmi.tuomas@gmail.com>
2021-04-05 22:07:44 +0300

    Add all workspaces to dynamic workspace list

--- i3_dynamic_workspace_names/configuration.py
+++ i3_dynamic_workspace_names/configuration.py
@@ -15,4 +15,4 @@ class RenameRule(Enum):
 rename_rule = RenameRule.FIRST_WINDOW
 
 # Workspaces which have dynamically changing icons.
-dynamic_workspace_names = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+dynamic_workspace_names = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
